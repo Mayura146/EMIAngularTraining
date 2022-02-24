9d80a7e968d9059c449c1f3275b22c67b4d8f531
mayura.sonar <mayura.sonar@outlook.com>
2022-02-24 10:30:03 +0530

    Routing

--- NewDemos_2022/RoutingDemoAngular12/src/app/Components/login/login.component.ts
+++ NewDemos_2022/RoutingDemoAngular12/src/app/Components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/Services/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+userName: string;
+password: string;
+alertMessage: 'Invalid Credentials!!';
+  constructor(private service: AuthenticationService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  public userLogin()
+  {
+    // this.userName=((document.getElementById('userName') as HTMLInputElement).value);
+    // this.password=((document.getElementById('password') as HTMLInputElement).value);
+    const result=this.service.authenticateUser(this.userName,this.password);
+    if(result===true)
+    {
+      this.router.navigate(['home']);
+    }
+
+    else
+    {
+      this.router.navigate(['customer'])
+    }
+  }
 }
