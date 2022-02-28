6e2a89f20db44a79ab24a8f166ac1971e3e4446b
mayura.sonar <mayura.sonar@outlook.com>
2022-02-28 11:10:34 +0530

    guards

---
+++ NewDemos_2022/RoutingDemoAngular12/src/app/RouteGuards/admin.guard.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminGuard implements CanActivate, CanActivateChild,CanDeactivate<SwitchComponent>  {
+
+  constructor(private service: AuthenticationService, private router: Router) {
+
+  }
+
+  canActivate(): boolean {
+    if (localStorage.getItem('currentuser') !== null) {
+      return true;
+    } else {
+      alert('Not Allowed to access this page!!');
+      this.router.navigate(['login']);
+    }
+
+  }
+  canActivateChild(route: ActivatedRouteSnapshot) :boolean{
+      if(this.service.ischildRights())
+      {
+        return true;
+      }
+
+      else
+      {
+        alert('you dont have permission to access this page!!');
+        this.router.navigateByUrl('/login');
+        return false;
+      }
+
+  }
+  canDeactivate():boolean
+  {
+    return window.confirm('Are you sure you want to leave this page!!');
+  }
+}
+
