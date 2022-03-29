ce2b0437197fe6bff12526e5eddb8583ae45f7e7
mayura.sonar <mayura.sonar@outlook.com>
2022-03-29 17:17:29 +0530

    grt

--- RouteGuard/karma.conf.js
+++ RouteGuard/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/role-routing'),
+      dir: require('path').join(__dirname, './coverage/route-guard'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- RouteGuard/src/app/app.component.spec.ts
+++ RouteGuard/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'RoleRouting'`, () => {
+  it(`should have as title 'RouteGuard'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('RoleRouting');
+    expect(app.title).toEqual('RouteGuard');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('RoleRouting app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('RouteGuard app is running!');
   });
 });
